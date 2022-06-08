9e66a30b88c571fd1151255449e330ac2e235569
EgorovM <egorov_michil@mail.ru>
2022-06-08 21:20:36 +0300

    inference model

--- models/egorov/ml_models_interface.py
+++ models/egorov/ml_models_interface.py
@@ -1,18 +1,24 @@
-import os.path
+import os
 from typing import Tuple
 
+import cv2
+
 import numpy as np
-from sklearn.utils.class_weight import compute_class_weight
+import pandas as pd
+import torch
+import torch.nn as nn
 
-import keras
-from keras import Sequential
-from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from utils.engine import train_one_epoch, evaluate
+from utils.datasets import MRIClassificationDataset
+from utils.transforms import get_transform
 
 
-class NNExample:
+DEFAULT_MODEL_WEIGHT = './models/class_segment_model.pth'
+
+
+class MRISegmentationClassificationNN:
     def __init__(self, image_size, batch_size,
-                 epoch=2, weight_path=None, **kwargs):
+                 epoch=2, weight_path=None, device='cuda', **kwargs):
 
         self.epoch: int = epoch
         self.batch_size: int = batch_size
@@ -20,99 +26,73 @@ class NNExample:
 
         self.weights_path: str = weight_path
 
+        self.device = device
         self.color_mode = kwargs.get('color_mode', 'grayscale')
         self.history = None
 
-        self.train_data_gen = ImageDataGenerator(
-            rescale=1. / 255,
-            shear_range=0.2,
-            width_shift_range=0.1,
-            height_shift_range=0.1,
-            brightness_range=(0.8, 1.2),
-            zoom_range=0.2,
-            horizontal_flip=True
+        self.train_data_gen = lambda df: MRIClassificationDataset(
+            df, get_transform(train=True)
         )
-        self.val_data_gen = ImageDataGenerator(rescale=1. / 255)
+        self.val_data_gen = lambda df: MRIClassificationDataset(
+            df, get_transform(train=False)
+        )
 
     def construct_model(self):
-        model = Sequential()
+        # Если появится torchvision, можно поменять
 
-        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(self.image_size[0], self.image_size[1], 1)))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
+        def weights_init(m):
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform(m.weight.data)
 
-        model.add(Conv2D(32, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
-
-        model.add(Conv2D(32, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
-
-        model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
-
-        model.add(Conv2D(128, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.5))
-
-        model.add(Flatten())
-
-        model.add(Dense(128, activation='relu'))
-        model.add(Dense(64, activation='relu'))
-        model.add(Dense(1, activation='sigmoid'))
-
-        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        weight_path = self.weights_path or DEFAULT_MODEL_WEIGHT
+        model = torch.jit.load(weight_path).to(self.device)
+        # reset the weights
+        model.apply(weights_init)
 
         return model
 
-    def create_data_iterator(self, generator, folder_path):
-        sub_folders = os.listdir(folder_path)
-        iterator = generator.flow_from_directory(
-            folder_path,
-            target_size=self.image_size,
-            color_mode=self.color_mode,
-            class_mode='binary' if len(sub_folders) == 2 else 'categorical',
-            batch_size=self.batch_size,
-            shuffle=True
+    def _read_df(self, df_path):
+        return pd.read_csv(df_path, sep='\t')
+    
+    def create_data_iterator(self, generator, df_path):
+        df = self._read_df(df_path)
+        dataset = generator(df)
+
+        data_loader = torch.utils.data.DataLoader(
+            dataset, 
+            batch_size=self.batch_size, 
+            collate_fn=lambda batch: tuple(zip(*batch))
         )
-        return iterator
+
+        return data_loader
 
     def get_callbacks(self) -> list:
-        from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+        return []
 
-        early_stop = EarlyStopping(monitor='val_loss', mode='min', patience=3)
-        learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss',
-                                                    patience=2, verbose=1, factor=0.3, min_lr=0.00001)
-
-        return [early_stop, learning_rate_reduction]
-
-    def fit(self, train_folder, val_folder=None):
+    def fit(self, train_df_path, val_df_path=None):
         # Data preparation and increase the amount of data set
-        gen_train = self.create_data_iterator(self.train_data_gen, train_folder)
+        gen_train = self.create_data_iterator(self.train_data_gen, train_df_path)
+
         gen_val = None
-        if val_folder:
-            gen_val = self.create_data_iterator(self.val_data_gen, val_folder)
+        if val_df_path:
+            gen_val = self.create_data_iterator(self.val_data_gen, val_df_path)
 
         model = self.construct_model()
 
-        weights = compute_class_weight(class_weight='balanced', classes=np.unique(gen_train.classes),
-                                       y=gen_train.classes)
-        class_weight = dict(zip(np.unique(gen_train.classes), weights))
+        params = [p for p in model.parameters() if p.requires_grad]
+        optimizer = torch.optim.SGD(params, lr=0.005,
+                                    momentum=0.9, weight_decay=0.0005)
 
-        callbacks_list = self.get_callbacks()
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                    step_size=3,
+                                                    gamma=0.1)
+        
         try:
-            steps_per_epoch = len(gen_train.filepaths) // self.batch_size
-            validation_steps = len(gen_val.filepaths) // self.batch_size
-            self.history = model.fit(
-                gen_train,
-                validation_data=gen_val,
-                epochs=self.epoch,
-                class_weight=class_weight,
-                steps_per_epoch=steps_per_epoch,
-                validation_steps=validation_steps,
-                callbacks=callbacks_list
-            )
+            for epoch in range(self.epoch):
+                train_one_epoch(model, optimizer, gen_train, self.device, epoch, 10)
+                lr_scheduler.step()
+                if gen_val:
+                    evaluate(model, gen_val, device=self.device)
         except Exception as e:
             raise Exception(f'Fitting error: {e}')
 
@@ -123,35 +103,43 @@ class NNExample:
 
         return self
 
-    def evaluate(self, test_folder, predict_column: str = 'predict', proba_column='proba'):
+    def _get_tumor_mask_and_class(self, img_path, model):
+        img_original = cv2.imread(img_path)
+        img, _ = get_transform(train=False)(img_original, {})
+        
+        with torch.no_grad():
+            prediction = model([img.to(self.device)])
+
+        overall_desease = None
+        overall_score = 0.5
+        total_mask = np.zeros_like(img_original)[:, :, 0]
+
+        for label, mask, score in zip(prediction[0]['labels'], prediction[0]['masks'], prediction[0]['scores']):
+            if label.item() != 2 and score.item() > 0.15 and (not overall_desease or label.item() == overall_desease):
+                mask = mask[0].mul(255).byte().cpu().numpy()
+                mask = cv2.threshold(mask, 100, 255, cv2.THRESH_BINARY)[1]
+                overall_desease = label.item()
+                overall_score = max(overall_score, score.item())
+
+                total_mask[mask == 255] = 255
+
+        return {'mask': total_mask, 'label': overall_desease or 0, 'proba': overall_score}
+
+    def evaluate(self, test_df_path, predict_column: str = 'predict', proba_column='proba'):
         if self.weights_path is None or not os.path.exists(self.weights_path):
             raise Exception(f'Weights were not found by path: {self.weights_path}')
+        
+        df = self._read_df(test_df_path)
+        model = self.construct_model()
 
-        model = keras.models.load_model(self.weights_path)
+        predict = []
+        proba = []
 
-        iterator = self.val_data_gen.flow_from_directory(
-            test_folder,
-            target_size=self.image_size,
-            color_mode=self.color_mode,
-            class_mode=None,
-            batch_size=self.batch_size,
-            shuffle=False
-        )
+        for image_path in df['image_path']:
+            result = self._get_tumor_mask_and_class(image_path, model)
+            predict.append(result['label'])
+            proba.append(result['proba'])
 
-        N = len(iterator.classes)
-        proba = model.predict(iterator, batch_size=self.batch_size, steps=N // self.batch_size + 1)
-
-        if proba.shape[0] > N:
-            # in case of odd number N / self.batch_size
-            proba = proba[:N]
-
-        if proba.shape[1] == 1:
-            proba = np.vstack((1 - proba, proba))
-
-        class_map = dict(map(lambda v: (v[1], v[0]), iterator.class_indices.items()))
-        predict = np.apply_along_axis(lambda v: class_map[v[0]], 1, np.argmax(proba, axis=1))
-
-        # Считается обратная вероятность (необходима для некоторых методов оценки)
-        proba = np.vstack((1 - predict, predict)).T
+            # можно сюда добавить маски: result['mask']
 
         return {predict_column: predict, proba_column: proba}
